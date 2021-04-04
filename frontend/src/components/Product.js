de452996f1ae693e6f94c49c40915a84001a7eff
Babatunde Onasanya <onasanyatunde67@gmail.com>
2021-04-04 19:26:24 +0100

    Feat: Home screen and product styling done

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,25 @@
+import "./Product.css";
+import { Link } from "react-router-dom";
+function Product() {
+  return (
+    <div className="product">
+      <img
+        src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
+        alt="Product Name"
+      />
+      <div className="product__info">
+        <p className="info__name"></p>
+        <p className="info__description">
+          loremloremloremloremloremloremlorem loremloremloremloremloremlorem
+          loremloremloremloremloremloremloremloremloremlorem
+        </p>
+        <p className="info__price">$499.99</p>
+        <Link to={`/product/${1111}`} className="info__button">
+          View
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,20 @@
 import "./HomeScreen.css";
+import Product from "../components/Product";
 
 const HomeScreen = () => {
-  return <div className="homescreen">homescreen</div>;
+  return (
+    <div className="homescreen">
+      <h2 className="homescreen__title">Latest Products</h2>
+      <div className="homescreen__products">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    </div>
+  );
 };
 
 export default HomeScreen;
