79814b81be8088c724a6c2aa850e4aa3cc69270a
Babatunde Onasanya <onasanyatunde67@gmail.com>
2021-04-04 19:48:08 +0100

    Product screen styling

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,46 @@
 import "./ProductScreen.css";
 const ProductScreen = () => {
-  return <div className="productscreen">productscreen</div>;
+  return (
+    <div className="productscreen">
+      <div className="productscreen__left">
+        <div className="left__image">
+          <img
+            src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
+            alt="product name"
+          />
+        </div>
+
+        <div className="left__info">
+          <p className="left__name">Product</p>
+          <p className="">Price: $499.99</p>
+          <p className="">Description: loremloremloremloremloremlorem</p>
+        </div>
+      </div>
+
+      <div className="productscreen__right">
+        <div className="right__info">
+          <p>
+            Price: <span>$499</span>
+          </p>
+          <p>
+            Status: <span>In Stock</span>
+          </p>
+          <p>
+            Qty
+            <select>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+          </p>
+          <p>
+            <button type="button"> Add To Cart</button>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductScreen;
