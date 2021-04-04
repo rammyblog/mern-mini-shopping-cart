dcc9455bfb1c10a4e503e10163496faa5641b7cb
Babatunde Onasanya <onasanyatunde67@gmail.com>
2021-04-04 18:01:00 +0100

    feat: Navbar css done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,10 +7,13 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 
+// Components
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <Router>
-      {/* Navbar */}
+      <Navbar />
       {/* SideDrawer */}
       {/* Backdrop */}
       <main>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,16 +5,29 @@ const Navbar = () => {
     <nav className="navbar">
       {/* Logo  */}
       <div className="navbar__logo">
-        <h2>MERn Shopping Cart</h2>
+        <h2>MERN Shopping Cart</h2>
       </div>
       {/* links */}
       <ul className="navbar__links">
         <li>
-          <Link to='/cart'></Link>
+          <Link to="/cart" className="cart__link">
+            <i className="fas fa-shopping-cart"></i>
+            <span>
+              Cart
+              <span className="cartlogo__badge">0</span>
+            </span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">Shop</Link>
         </li>
       </ul>
 
-      {/* hamburger menu */}
+      <div className="hamburger__menu">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </nav>
   );
 };
