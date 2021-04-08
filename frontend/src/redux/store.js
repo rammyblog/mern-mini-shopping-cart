5fe21c8c522c531ab9dd93573c03245dcc5c2dd2
Babatunde Onasanya <onasanyatunde67@gmail.com>
2021-04-08 23:18:15 +0100

    Persist data from local storage

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -16,8 +16,17 @@ const reducer = combineReducers({
 
 // It helps make async request in the actions
 const middleware = [thunk];
+const cartFromLocalStorage = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : [];
+const INITIAL_STATE = {
+  cart: {
+    cartItems: cartFromLocalStorage,
+  },
+};
 const store = createStore(
   reducer,
+  INITIAL_STATE,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
